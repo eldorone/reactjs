b4eb30d5073aa79ee1f35b887ccd6143377ebeb2
Eldor Alikuvvatov <eldoralikuvvatov@gmail.com>
2022-11-30 20:34:42 +0900

    second lesson finished

--- src/index.js
+++ src/index.js
@@ -1,22 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Student from './Student.jsx'
+import Fruits from './Fruits.jsx';
 
-const studentsInfo = [
-  { id: 18012704, name: 'Eldor' },
-  { id: 18012705, name: 'Cris' },
-  { id: 18012706, name: 'Leonel' },
-  { id: 18012707, name: 'Jamal' },
-  { id: 18012708, name: 'Gavi' },
-  { id: 18012709, name: 'Diogo' },
-  { id: 18012710, name: 'Alex' }
+const fruits = [
+  { url: 'https://bit.ly/3ARrT5J', name: 'Watermelon' },
+  { url: 'https://bit.ly/3ity3SY', name: 'Orange' },
+  { url: 'https://bit.ly/3OMGzsm', name: 'Cherry' },
+  { url: 'https://bit.ly/3AVuGuF', name: 'Lemon' },
+  { url: 'https://bit.ly/3AYqytI', name: 'Blueberry' },
+
+  { url: 'https://bit.ly/3EMvRxD', name: 'Banana' },
+  { url: 'https://bit.ly/3VC5jpt', name: 'Peach' },
+  { url: 'https://bit.ly/3Uiy5KM', name: 'Coconut' },
+  { url: 'https://bit.ly/2DfPXBA', name: 'Papaya' },
+  { url: 'https://bit.ly/3AYMogN', name: 'Dragon fruit' },
+
+  { url: 'https://bit.ly/3Fg179Q', name: 'Grape' },
+  { url: 'https://bit.ly/3Vp0ZdB', name: 'Mango' },
+  { url: 'https://shutr.bz/3u9HsSs', name: 'Mangosteen' },
+  { url: 'https://bit.ly/3ucwtYv', name: 'Durian' },
+  { url: 'https://bit.ly/3UrWC0g', name: 'Avocado' }
 ]
 
 ReactDOM.render(
   <React.StrictMode>
-    {studentsInfo.map((value) => {
-      return <Student data={value} />;
+    {fruits.map((value) => {
+      return <Fruits data={value} />;
     })}
   </React.StrictMode>,
 
